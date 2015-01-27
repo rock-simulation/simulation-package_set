d09c7eb2998a62d5f2c3cff045852dde4e88303c
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2015-01-27 16:39:37 +0100

    fixed mars_core for old branch

--- overrides.rb
+++ overrides.rb
@@ -1,6 +1,6 @@
 
 #TODO roll this commit message during release-time
-only_in_flavor 'master' do
+only_in_flavor 'next','stable' do
     setup_package 'simulation/orogen/mars_core' do |pkg|
         pkg.importer.branch = "depricated-naming"
     end  

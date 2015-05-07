d0c1a9e89045f5cdeeffdd3f1fd3206ceb4ab450
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2015-05-07 10:38:40 +0200

    Updated mars regarding the new release policy

--- overrides.rb
+++ overrides.rb
@@ -1,7 +1,1 @@
 
-#TODO roll this commit message during release-time
-only_in_flavor 'next','stable' do
-    setup_package 'simulation/orogen/mars_core' do |pkg|
-        pkg.importer.branch = "depricated-naming"
-    end  
-end
